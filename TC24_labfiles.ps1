8c3e463edd69d30ebe1f033e41713c3a93912be4
James-Burnham <James.Burnham@learnondemandsystems.com>
2024-04-28 12:32:10 -0700

    Update TC24_labfiles.ps1

--- TC24_labfiles.ps1
+++ TC24_labfiles.ps1
@@ -4,18 +4,23 @@ param (
 
 $scriptPath = 'C:\Users\Admin\Documents\download_files.ps1'
 $filePath = "https://tc24labfiles.s3.us-west-2.amazonaws.com/$ID/VM+Files/VM+Files.zip"
+
 $scriptContent = @"
 Import-Module -Name Microsoft.PowerShell.Archive
  
-`$url = $filePath
+`$url = '{0}'
 `$destination = 'C:\Users\Admin\VM Files.zip'
  
 Invoke-WebRequest -Uri `$url -OutFile `$destination -Verbose
  
 Expand-Archive -Path `$destination -DestinationPath 'C:\Users\Admin\Desktop' -Verbose
 "@
- 
-Set-Content -Path $scriptPath -Value $scriptContent
- 
-pwsh $scriptPath
 
+# Insert the filePath into the script content where {0} is the placeholder
+$scriptContent = $scriptContent -f $filePath
+
+# Write the content to the new script file
+Set-Content -Path $scriptPath -Value $scriptContent
+
+# Execute the newly created script
+pwsh $scriptPath
