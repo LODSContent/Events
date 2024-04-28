15814e1320edcbfb1b864871872f5f3f33820ec0
James-Burnham <James.Burnham@learnondemandsystems.com>
2024-04-28 12:23:15 -0700

    Update TC24_labfiles.ps1
    Parameterized

--- TC24_labfiles.ps1
+++ TC24_labfiles.ps1
@@ -1,8 +1,13 @@
+param (
+    [string]$ID = "1796"
+)
+
 $scriptPath = 'C:\Users\Admin\Documents\download_files.ps1'
+$filePath = "https://tc24labfiles.s3.us-west-2.amazonaws.com/$ID/VM+Files/VM+Files.zip"
 $scriptContent = @"
 Import-Module -Name Microsoft.PowerShell.Archive
  
-`$url = 'https://tc24labfiles.s3.us-west-2.amazonaws.com/1796/VM+Files/VM+Files.zip'
+`$url = $filePath
 `$destination = 'C:\Users\Admin\VM Files.zip'
  
 Invoke-WebRequest -Uri `$url -OutFile `$destination -Verbose
@@ -13,3 +18,4 @@ Expand-Archive -Path `$destination -DestinationPath 'C:\Users\Admin\Desktop' -Ve
 Set-Content -Path $scriptPath -Value $scriptContent
  
 pwsh $scriptPath
+
