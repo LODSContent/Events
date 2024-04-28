042d2a9809e3069c2ff23cec9203b0455e5b5b22
James-Burnham <James.Burnham@learnondemandsystems.com>
2024-04-28 11:26:32 -0700

    Create TC24_labfiles.ps1

---
+++ TC24_labfiles.ps1
@@ -0,0 +1,15 @@
+$scriptPath = 'C:\Users\Admin\Documents\download_files.ps1'
+$scriptContent = @"
+Import-Module -Name Microsoft.PowerShell.Archive
+ 
+`$url = 'https://tc24labfiles.s3.us-west-2.amazonaws.com/1796/VM+Files/Desktop/VM+Files.zip'
+`$destination = 'C:\Users\Admin\VM Files.zip'
+ 
+Invoke-WebRequest -Uri `$url -OutFile `$destination -Verbose
+ 
+Expand-Archive -Path `$destination -DestinationPath 'C:\Users\Admin\Desktop' -Verbose
+"@
+ 
+Set-Content -Path $scriptPath -Value $scriptContent
+ 
+pwsh $scriptPath
